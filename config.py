3962f746e5debb23d58db31bf7f383091ced6730
Andriy Stepanenko <ast.university@hotmail.com>
2021-11-28 15:43:19 +0200

    Changed config.py. Added diff config classes

--- config.py
+++ config.py
@@ -8,12 +8,33 @@ class Config:
     """
     Configuration class
     """
+    # Flask-realted config
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_very_secret_key'
+
+    # DB credentials
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST')
     DB_PORT = os.environ.get('DB_PORT')
     DB_NAME = os.environ.get('DB_NAME')
+
+    # SQLAlchemy config
     SQLALCHEMY_DATABASE_URI = \
         f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_very_secret_key'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+
+    'default': DevelopmentConfig
+}
