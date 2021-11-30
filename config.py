01e03a5663b918bf3345dc45bfcc56da837399d7
Andriy Stepanenko <ast.university@hotmail.com>
2021-11-30 12:31:34 +0200

    Refined the config and dotenv files

--- config.py
+++ config.py
@@ -1,22 +1,20 @@
-import os
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+from os import environ
 
 
 class Config:
-    """
-    Configuration class
-    """
+    """Base configuration class"""
+
     # Flask-realted config
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_very_secret_key'
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV', 'development')
+    SECRET_KEY = environ.get('SECRET_KEY', 'my_very_secret_key')
 
     # DB credentials
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
-    DB_HOST = os.environ.get('DB_HOST')
-    DB_PORT = os.environ.get('DB_PORT')
-    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = environ.get('DB_USER')
+    DB_PASSWORD = environ.get('DB_PASSWORD')
+    DB_HOST = environ.get('DB_HOST')
+    DB_PORT = environ.get('DB_PORT')
+    DB_NAME = environ.get('DB_NAME')
 
     # SQLAlchemy config
     SQLALCHEMY_DATABASE_URI = \
@@ -24,24 +22,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # MAIL
-    MAIL_SERVER=os.environ.get('MAIL_SERVER')
-    MAIL_PORT=os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
-
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-
-    'default': DevelopmentConfig
-}
+    MAIL_SERVER=environ.get('MAIL_SERVER')
+    MAIL_PORT=environ.get('MAIL_PORT')
+    MAIL_USE_TLS=environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME=environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=environ.get('MAIL_PASSWORD')
