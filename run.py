f3a522d5ea0fbfadba636fae0ff1e4b94eba28f5
Andriy Stepanenko <ast.university@hotmail.com>
2022-01-05 13:49:54 +0200

    Minor change in run.py

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ from os import getenv
 from dotenv import load_dotenv
 
 
+load_dotenv()
+app = create_app(getenv('FLASK_CONFIG'))
+
+
 if __name__ == '__main__':
-    load_dotenv()
-    app = create_app(getenv('FLASK_CONFIG'))
     app.run()    
